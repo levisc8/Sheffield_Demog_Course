48de6a2faa73d7db8d36a2e520e42bd234450242
levisc8 <levisc8@gmail.com>
2018-01-26 10:35:28 +0000

    Age x size IPM part 2

--- R/Age_Size_IPM.R
+++ R/Age_Size_IPM.R
@@ -1,4 +1,9 @@
 # Age x Size IPM
+# Presentation available at: 
+# http://rpubs.com/dzchilds/age-size-ipm-exercise
+
+# Further details stored in :
+# C:/Users/sl13sise/Dropbox/ATS 2018 participant folder/25.1.18/Soay_Age_Size/general_ipm.html
 
 # Sam Levin
 # 1/25/18
@@ -6,6 +11,7 @@
 rm(list = ls())
 library(car)
 library(dplyr)
+library(Matrix)
 
 source('R/WD_Paths.R')
 
@@ -14,6 +20,8 @@ setwd(data.wd)
 data <- read.csv('25.1.18/Soay_Age_Size/soay_demog_data.csv',
                  stringsAsFactors = FALSE) %>%
   as_tibble()
+
+source('23.1.18/Rees/R code and Data/MatrixImage.R')
 
 setwd(old.wd)
 
@@ -160,14 +168,138 @@ F_z1z <- function(z, z1, a, m_par) {
   return(out/2)
 }
 
+# Create parameters describing each one. Below is my attempt at describing this,
+# but Dylan has an alternative way. I'll construct those further down and try 
+# to compare the two approaches to see how bad I am at doing this on my own
+
+#system.time({
+nMeshP <- 250
 L <- min(data$z) * 0.9
 U <- max(data$z) * 1.1
-MeshPoints <- seq(L, U, length.out = 250)
+MeshPoints <- seq(L, U, length.out = nMeshP)
 BinWidth <- MeshPoints[2] - MeshPoints[1]
 Ages <- seq(0, max(data$a, na.rm = TRUE), by = 1)
 
-library(Matrix)
-GMat <- BinWidth * outer(MeshPoints,
-                         MeshPoints,
-                         FUN = g_z1z)
+PMatList <- list()
+FMatList <- list()
+for(Age in Ages) {
+  GMat <- BinWidth * outer(MeshPoints,
+                           MeshPoints,
+                           FUN = g_z1z,
+                           a = Age,
+                           m_par = m_par)
+  GMat <- GMat / matrix(as.vector(apply(GMat,
+                                        2,
+                                        sum)),
+                        nrow = nMeshP,
+                        ncol = nMeshP,
+                        byrow = TRUE)
+  
+  PMatList[[Age + 1]] <- GMat %*% diag(s_z(MeshPoints,
+                                           a = Age, 
+                                           m_par = m_par))
+  
+  FMatList[[Age + 1]] <- F_z1z(MeshPoints,
+                               MeshPoints,
+                               a = Age,
+                               m_par = m_par)
+  
+}
+#})
+# Now, Dylan's implementation!
+# Calculate the mesh points, mesh width and store with upper/lower 
+# bounds and max age
+
+#system.time({
+mk_intpar <- function(m, L, U, M) {
+  h <- (U - L) / m
+  meshpts  <-  L + ((1:m) - 1/2) * h
+  na <- M + 2
+  return( list(meshpts = meshpts, M = M, na = na, h = h, m = m) )
+}
+
+
+# make initial state distribution (uniform size, age 0 only)
+mk_init_nt0 <- function(i_par) {
+  nt <- lapply(seq_len(i_par$na), function(x) rep(0, i_par$m))
+  nt[[1]] <- rep(1, i_par$m) / (i_par$h * i_par$m)
+  return(nt)
+}
+
+# Build the list of age/process specific kernels + store the 
+# integration parameters in the same list
+mk_age_IPM <- function(i_par, m_par) {
+  within(i_par, {
+    F <- P <- list()
+    for (ia in seq_len(na)) {
+      F[[ia]] <- outer(meshpts, meshpts, F_z1z, a = ia-1, m_par = m_par) * h
+      P[[ia]] <- outer(meshpts, meshpts, P_z1z, a = ia-1, m_par = m_par) * h
+    }
+    rm(ia)
+  })
+}
+
+# Create structural parameters
+StructPars <- mk_intpar(250, # Number of meshpoints
+                        min(data$z) * 0.9, # smallest sizes
+                        max(data$z) * 1.1, # largest sizes
+                        M = max(data$a)) # maximum allowable age
+
+init_n0 <- mk_init_nt0(i_par = StructPars)
+
+IpmKernels <- mk_age_IPM(i_par = StructPars,
+                         m_par = m_par)
+
+
+#})
+# System.time results: 
+# Dylan: 0.53 second
+# Sam: 0.27 seconds
+# Haha!
+
+# Now right and left iteration. Right iteration creates the right
+# eigenvector, left iteration creates the left eigenvector
+r_iter <- function(x, F, P) {
+  na <- length(x)
+  xnew <- list(0)
+  # multiply 
+  for (ia in seq_len(na)) {
+    xnew[[1]] <- (xnew[[1]] + F[[ia]] %*% x[[ia]])[,,drop=TRUE]
+  }
+  for (ia in seq_len(na-1)) {
+    xnew[[ia+1]] <- (P[[ia]] %*% x[[ia]])[,,drop=TRUE]
+  }
+  xnew[[na]] <- xnew[[na]] + (P[[na]] %*% x[[na]])[,,drop=TRUE]
+  return(xnew)
+}
+
+# See what the hell this thing is doing
+FirstIt <- r_iter(x = init_n0, F = IpmKernels$F, P = IpmKernels$P)
+
+# It's moved all individuals from our initial vector (which is just a uniform
+# distribution of individuals at Age 0), on to the next year. So I guess
+# we just run this for a lot of years and see what happens
+
+YearSeq <- seq(1, 100, 1)
+Output <- list()
+Lambdas <- list()
+
+for(i in YearSeq) {
+  if(i == 1) {
+    New_n0 <- r_iter(init_n0, 
+                     F = IpmKernels$F, 
+                     P = IpmKernels$P)
+    Lambda <- sum(unlist(New_n0)) / sum(unlist(init_n0))
+  } else {
+    New_n0 <- r_iter(New_n0, 
+                     F = IpmKernels$F, 
+                     P = IpmKernels$P)
+    Lambda <- sum(unlist(New_n0)) / sum(unlist(Output[[i-1]]))
+  }  
+  Output[[i]] <- New_n0
+  Lambdas[[i]] <- Lambda
+  
+  cat('lambda: ', Lambda, '\n')
+}
+
 
